32dc3199aa75c127a594e606fb8630b922336b35
prayoga <prayogamaulanaputras@gmail.com>
2021-10-18 00:14:12 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderWormth />
+    <HeroWormth />
+    <BannerWormth />
+    <InstaWormth />
+    <PartnerWormth />
+    <FooterWormth />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderWormth from "../components/HeaderWormth.vue";
+import HeroWormth from "../components/HeroWormth.vue";
+import BannerWormth from "../components/BannerWormth.vue";
+import InstaWormth from "../components/InstaWormth.vue";
+import PartnerWormth from "../components/PartnerWormth.vue";
+
+import FooterWormth from "../components/FooterWormth.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderWormth,
+    HeroWormth,
+    BannerWormth,
+    InstaWormth,
+    PartnerWormth,
+    FooterWormth,
+  },
+};
+</script>
